33a663418541450a0126376e5ba7b680c05e06b0
KevinLim62 <70590478+KevinLim62@users.noreply.github.com>
2023-07-01 21:32:28 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   // trailingSlash: true,
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
-
+  basePath: '/nextapp_testing',
 }
 
 module.exports = nextConfig
