f8a96ed0461ce526f496ca46ebe2ec84b8a8b125
KevinLim62 <70590478+KevinLim62@users.noreply.github.com>
2023-07-01 21:55:32 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  // trailingSlash: true,
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
   basePath: '/nextapp_testing',
   assetPrefix: '/nextapp_testing/',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
